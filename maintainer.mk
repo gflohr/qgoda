5879d8b33067a2afe6910809f1ef24d5276bb058
Guido Flohr <guido.flohr@cantanea.com>
2016-12-09 09:06:25 +0200

    build liquid parser

--- maintainer.mk
+++ maintainer.mk
@@ -1,6 +1,12 @@
-all :: ReleaseNotes
+all :: ReleaseNotes lib/Qgoda/Parser/Liquid.pm
 
 Makefile: maintainer.mk
 
 ReleaseNotes: NEWS
 	cat NEWS >$@
+
+SHELL := env YAPP=$(YAPP) $(SHELL)
+YAPP ?= "$(INSTALLSITEBIN)/yapp"
+
+lib/Qgoda/Parser/Liquid.pm: Liquid.y
+	$(YAPP) -m Qgoda::Parser::Liquid -s -o $@ $<
