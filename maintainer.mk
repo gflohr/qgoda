3276abe86297bcbb40c8be9eeca22bee927c8414
Guido Flohr <guido.flohr@cantanea.com>
2017-10-29 12:26:52 +0200

    saved current init state, begin rewrite

--- maintainer.mk
+++ maintainer.mk
@@ -14,9 +14,9 @@ lib/Qgoda/Migrator/Jekyll/LiquidParser.pm: LiquidParser.y
 Init_DEPS = \
         lib/Qgoda/Init.pm.in \
         maintainer.mk \
-        data/* \
-        data/*/* \
-        data/*/*/* \
-        data/*/*/*/*
+        share/* \
+        share/*/* \
+        share/*/*/* \
+        share/*/*/*/*
 lib/Qgoda/Init.pm: $(Init_DEPS) 
-	perl -pe 's{\@([-_a-z0-9./]+)\@}{`cat data/$$1`}gxe' lib/Qgoda/Init.pm.in >$@
+	perl -pe 's{\@([-_a-z0-9./]+)\@}{`cat share/$$1`}gxe' lib/Qgoda/Init.pm.in >$@
