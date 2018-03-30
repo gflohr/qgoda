18e40c7090860b6418796d0a61a4a2cd4459d804
Guido Flohr <guido.flohr@cantanea.com>
2018-03-30 08:59:09 +0300

    disabled yapp

--- maintainer.mk
+++ maintainer.mk
@@ -5,8 +5,9 @@ Makefile: maintainer.mk
 ReleaseNotes: NEWS
 	cat NEWS >$@
 
-SHELL := env YAPP=$(YAPP) $(SHELL)
-YAPP ?= "$(INSTALLSITEBIN)/yapp"
-
-lib/Qgoda/Migrator/Jekyll/LiquidParser.pm: LiquidParser.y
-	$(YAPP) -v -m Qgoda::Migrator::Jekyll::LiquidParser -s -o $@ $<
+# Temporary disabled.
+#SHELL := env YAPP=$(YAPP) $(SHELL)
+#YAPP ?= "$(INSTALLSITEBIN)/yapp"
+#
+#lib/Qgoda/Migrator/Jekyll/LiquidParser.pm: LiquidParser.y
+#	$(YAPP) -v -m Qgoda::Migrator::Jekyll::LiquidParser -s -o $@ $<
