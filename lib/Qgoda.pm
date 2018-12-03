7bb7e7e2c92db19da9cb24b8a68f0fb0fcfc84d2
Guido Flohr <guido.flohr@cantanea.com>
2018-12-03 19:24:58 +0200

    bumped version number to 0.9.6

--- lib/Qgoda.pm
+++ lib/Qgoda.pm
@@ -17,7 +17,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 package Qgoda;
-$Qgoda::VERSION = '0.9.5';
+$Qgoda::VERSION = '0.9.6';
 use common::sense;
 
 # FIXME! This assumes that we are a top-level package. Instead,
