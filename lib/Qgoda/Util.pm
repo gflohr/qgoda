b0f415346339481fbda23309b702ef876f9ca602
Guido Flohr <guido.flohr@cantanea.com>
2025-02-14 16:09:22 +0200

    remove invalid fixes

--- lib/Qgoda/Util.pm
+++ lib/Qgoda/Util.pm
@@ -130,7 +130,6 @@ sub write_file($$) {
 
 	my ($volume, $directory) = File::Spec->splitpath($path);
 	my $dirpart = File::Spec->catpath($volume, $directory);
-	$dirpart =~ s{[/\\]$}{}; # Necessary under Windows.
 
 	if (!empty $dirpart && !-e $dirpart) {
 		make_path $dirpart;
